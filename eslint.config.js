6bf509610b2cca6d497da5f9172f68c40e374bd3
StarEclipse <kadaj0706t@gmail.com>
2026-02-06 11:46:52 +0800

    style: 統一 ESLint 設定格式並新增規則
    將語句結尾統一加上分號，並關閉 react-hooks/set-state-in-effect 規則以允許在 useEffect 中使用 setState 取得資料。

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,8 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -24,6 +24,8 @@ export default defineConfig([
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      // Data fetching in useEffect is a valid use case for setState
+      'react-hooks/set-state-in-effect': 'off',
     },
   },
-])
+]);
